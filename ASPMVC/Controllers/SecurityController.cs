5637995eb8e5dbc2c83f704a72fd3b17909ca495
Mateusz Krawczyk <Mateusz Krawczyk>
2018-03-05 21:09:33 +0100

    last comment

--- ASPMVC/Controllers/SecurityController.cs
+++ ASPMVC/Controllers/SecurityController.cs
@@ -23,11 +23,29 @@ using System.Web.Mvc;
 ///     
 ///     Passive Injection - np. wpisanie do komentarza kodu JavaScript (<script>Coś złego</script>) 
 /// 
+///     Istnieje równieź wstrzyknięcie kodu JS który może dostać się do cookies i wysłać z naszej strony jakiś formularz logowania
+///     z wartościami z cookies. 
+///         Nowe przeglądarki mają właściwość ciasteczek po stronie HTTPonly, czyli nie można ich użyć w kodzie JS.
+///         Response.Cookies["MySecretCookie"].HttpOnly=true;
+/// 
 /// NIGDY NIE UFAJ DANYM OD UŻYTKOWNIKA!!!
 /// NIE WYŚWIETLAJ DANYCH UŻYTKOWNIKA BEZ ENKODOWANIA - domyślnie zwracane dane są enkodowane. Można się to wyłączyć
 /// przez [ValidateInput(false)], Http.Raw, AntXSS library.
 /// 
 /// 
+/// Cross Site Request Forgery (CSRF)
+/// Wiele stron zapamiętuje użytkownika. Atak polega na wykorzystaniu zalogowania
+/// - Wysłanie linku mailem typu http://my-bank.com?function=transfer&amount=1000&toAccount=1232332
+/// - Wstawienie linku pod komentarzami banku(XSS) <img src="http://my-bank.com?function=transfer&amount=1000&toAccount=1232332"/>
+/// 
+/// 
+/// Komunikaty o błedach - ustawić <compilation debug="false"> aby ukryć informacje o błedach serwera
+/// 
+/// 
+/// Zabezpieczenie kontrolerów [Authorize]
+/// 
+/// 
+/// Zabezpieczenie publicznych metod - [NonAction]
 /// </summary>
 namespace ASPMVC.Controllers
 {
@@ -43,6 +61,7 @@ namespace ASPMVC.Controllers
         /// Provider - where db of users are, default - Entity Framework.
         /// </summary>
         /// <returns></returns>
+        /// 
         public ActionResult Authentication()
         {
             if(User.Identity.IsAuthenticated)
@@ -78,6 +97,8 @@ namespace ASPMVC.Controllers
         }
 
         [ValidateInput(false)] //We can add strings like "<div><form><input name="password...."
+        [HttpPost] //Should be POST request
+        [ValidateAntiForgeryToken] //Check if form has hidden input with token (anti XSS/CSRF)
         public ActionResult AddQuestion(Question question)
         {
             //AntiXSS library - get only safe html fragment (without <script>, <forms> etc.)
@@ -86,6 +107,54 @@ namespace ASPMVC.Controllers
             SecurityController.Questions.Add(question);
             return RedirectToAction(nameof(Index));
         }
+
+
+        // CSRF - attack example. Let's suppose that user will get email with link to this page:
+        //
+        // <body onload="document.getElementById("fm1").submit()"> <- automatically submitted 
+        //  <form id="fm1" action="http://MY-PAGE.com/Seciurity/SubmitUpdate" method="post"> <- post action to our page 
+        //      <input name="email" value="hacker@email.com" /> <- hacker's email
+        //      <input name="hobby" value="hacker hobby" /> <- hacker's hobby
+        //  </form>
+        // </body>
+        //
+        // It executes this action, because user is saved by broswer (remember me).
+        public ActionResult SubmitUpdate()
+        {
+            UserProfile profile = GetUserProfile();
+            profile.Email = Request.Form["Email"];
+            profile.Hobby = Request.Form["Hobby"];
+            SaveChanges(profile);
+
+            ViewBag.Message = "Your profile was updated";
+
+            return View();
+        }
+        // Solution - 
+        //      Always use POST to this types of actions.
+        //      Put hidden input in every type of login pages (hidden token)
+        //          [ValidateAntiForgeryToken] in post action
+        //          @HTML.AntiForgeryToken() in form
+        //      Referer - check source of POST request (which page sends POST query)
+
+
+        //Just example...
+        public static UserProfile GetUserProfile()
+        {
+            return new UserProfile(); //Just example...
+        }
+
+        public static void SaveChanges(UserProfile userProfile)
+        { }
+
+    }
+
+    public class UserProfile
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Hobby { get; set; }
+
 
     }
 }
--- ASPMVC/README.txt
+++ ASPMVC/README.txt
@@ -3,3 +3,27 @@
 Global.asax - plik wejsciowy serwera. Definiowanie tablicy routingu
 
 RouteConfig.cs - definiowanie zapisu URL ../{x}/{y}-{z}
+
+
+Wdrażanie aplikacji:
+	Hostowanie na komputerze(Win):
+		- Windows Server (może być zwykły Windows)
+		- Zainstalowana usługa IIS
+		- Baza danych np. SQL Express
+	Hostowanie na komputerze(Inne):
+		- Linux
+		- Mono
+		- Apache
+	Usługa hostingowa:
+		- Azure
+
+	Paczka do wdrażania:
+		- kompilacja *.cs do *.dll (pliki CSHTML nie)
+		- usunięcie debug="true" z web.config
+		- podmiana connection string do bazy danych
+		- wyłącznie custom error (zamiana na domyślną strone error.html)
+
+	Możemy ustawić własne ustawienia konfiguracji. Web.config ma również pliki Web.Debug.config oraz Web.Release.config które
+	mogą posłużyć jako transformatory xdt. W tych miejscach możemy transformatować connection stringi, usuwać niektóre linie kodu
+	z Web.config w zależności od konfiguracji (Debug/Release lub własny). Sprawdź 'Web.Release - IIS.config' gdzie zamiana następuje na 
+	DbCourseEntities (Preview Transform). 
